98a1fa6aefdce159e666f6b8dec1b0f0706c9cce
MohcineSafi <safi.mouhcine02@gmail.com>
2024-07-31 00:04:18 +0100

    feat: Add enemy movement and collision detection

---
+++ Scripts/enemy2.gd
@@ -0,0 +1,47 @@
+extends Area2D
+export var speed = 100
+export var distance = 100
+export var spin = 3
+onready var startPosition: float = position.y
+onready var target_dist: float = position.y + distance
+
+func _ready():
+	pass
+#create a function called move to
+func move_to(currentPosition, target, moving):
+	var newPosition = currentPosition
+	#are we moving right
+	if newPosition < target:
+		newPosition += moving
+		#have we reached
+		if newPosition > target:
+			newPosition = target
+	#else are we moving left
+	else:
+		newPosition -= moving
+		#have reached the left side?
+		if newPosition < target:
+			newPosition = target
+	return newPosition
+#implement the move to function
+func _physics_process(delta):
+	position.y = move_to(position.y, target_dist, speed * delta)
+	#have we reached the target?
+	if position.y == target_dist:
+		#successfully reached the left side
+		if target_dist == startPosition:
+			#start moving right
+			target_dist = position.y + distance
+		else:
+			target_dist = startPosition
+	#spinning
+	if position.y < target_dist:
+		rotation_degrees += spin
+	else:
+		rotation_degrees -= spin
+		
+#detect and kill the player if hit
+func _on_enemy_body_entered(body):
+	if body == ($"../../player"):
+		get_tree().reload_current_scene()
+		
